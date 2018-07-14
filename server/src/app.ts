8e3a244b3d52cca38ead7cabafa0248e98196807
Tyler Zhang <tztylerzhang@gmail.com>
2018-07-14 03:09:28 -0400

    initialize express app with empty organization controller

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,10 @@
 import { createConnection } from 'typeorm';
+import { get } from 'lodash';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { useExpressServer, useContainer } from 'routing-controllers';
 import { databaseConfig, log } from './config';
+import { join } from 'path';
 
 
 export async function launch() {
@@ -7,6 +12,23 @@ export async function launch() {
    * Connect to database
    */
   await createConnection(databaseConfig);
-
   log.info(`Connected to database`);
+
+  /**
+   * Start express server
+   */
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  useExpressServer(app, {
+    controllers: [join(__dirname, 'controllers', '*{ts,js}')],
+    routePrefix: '/api',
+    classTransformer: true
+  });
+
+  const port = get(process.env, 'PORT', 80);
+  app.listen(port);
+
+  log.info(`Running web app on port ${port}`);
 }
---
+++ server/src/controllers/OrganizationController.ts
@@ -0,0 +1,5 @@
+import { Controller } from "routing-controllers";
+
+@Controller('/organizations')
+export class OrganizationController {
+}

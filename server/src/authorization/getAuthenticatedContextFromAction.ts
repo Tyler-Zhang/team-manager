eb1642923f6734052a1e8ac35870f4ac3464814b
Tyler Zhang <tztylerzhang@gmail.com>
2018-07-22 12:16:34 -0400

    use new style of operation calling

--- server/src/authorization/getAuthenticatedContextFromAction.ts
+++ server/src/authorization/getAuthenticatedContextFromAction.ts
@@ -5,5 +5,5 @@ import { FromToken } from "../operations/AuthenticatedContext/FromToken";
 export function getAuthenticatedContextFromAction(action: Action) {
   const jwt = get(action.request, 'headers.authorization');
 
-  return new FromToken({ jwtToken: jwt }).run();
+  return FromToken.run({ jwtToken: jwt });
 }

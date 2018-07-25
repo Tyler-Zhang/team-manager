da83e93f98f778eba80717b896ac1797433bfb9d
Tyler Zhang <tztylerzhang@gmail.com>
2018-07-25 19:04:10 -0400

    operations/authenticatedContext/fromMember specs

---
+++ server/src/operations/AuthenticatedContext/FromMember.spec.ts
@@ -0,0 +1,17 @@
+import 'mocha';
+import { Factories } from '../../specs';
+import { expect } from 'chai';
+import { AuthenticatedContextOperations } from '..';
+
+describe('operations/AuthenticatedContext/FromMember', () => {
+  const member = Factories.member.build();
+  const run = () => AuthenticatedContextOperations.FromMember.run({ member })
+
+  it('sets the fields correctly', async () => {
+    const authContext = await run();
+
+    expect(authContext.getAuthority()).to.equal(member.authority)
+    expect(authContext.getMemberId()).to.equal(member.id)
+    expect(authContext.getOrganizationId()).to.equal(member.organizationId)
+  })
+})

2237b02033cb5b765c02a4cb2419fdfcc8716cc4
Tyler Zhang <tztylerzhang@gmail.com>
2018-08-06 14:09:18 -0400

    add new team reducer

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,13 +1,19 @@
 import { combineReducers } from 'redux';
 import authenticationReducer, { IAuthenticationState } from './authReducer';
+import membersReducer, { IMembersState } from './membersReducer';
 import ormReducer from './ormReducer/ormReducer';
+import teamsReducer, { ITeamsState } from './teamsReducer';
 
 export interface IState {
   orm: any;
   authentication: IAuthenticationState;
+  members: IMembersState;
+  teams: ITeamsState;
 }
 
 export const rootReducer = combineReducers({
   orm: ormReducer,
-  authentication: authenticationReducer
+  authentication: authenticationReducer,
+  members: membersReducer,
+  teams: teamsReducer
 });
---
+++ client/src/store/reducers/teamsReducer.ts
@@ -0,0 +1,113 @@
+import { createAction } from 'typesafe-actions';
+import { AppReducer, mapReducers, noOpReducer, ReducerMap } from '../../lib/ReduxHelpers';
+import { ITeam, ProtoModel } from '../../models';
+
+/* ------------- Types and Action Creators ------------- */
+export interface ITeamPayloadCreate {
+  team: ProtoModel<ITeam>
+}
+
+const actions = {
+  teamsQuery: createAction('teams/QUERY'),
+  teamsLoadStart: createAction('teams/LOAD_START'),
+  teamsLoadError: createAction('teams/LOAD_ERROR', resolve => (payload: Error) => resolve(payload)),
+  teamsLoadSuccess: createAction('teams/LOAD_SUCCESS'),
+  teamsCreate: createAction('teams/CREATE', resolve => (payload: ITeamPayloadCreate) => resolve(payload)),
+  teamsCreateStart: createAction('teams/CREATE_START'),
+  teamsCreateError: createAction('teams/CREATE_ERROR', resolve => (payload: Error) => resolve(payload)),
+  teamsCreateSuccess: createAction('teams/CREATE_SUCCESS')
+};
+
+
+export const TeamActions = actions;
+
+export interface ITeamsState {
+  fetchError: Error | null;
+  isFetching: boolean;
+  isCreating: boolean;
+  createError: Error | null;
+}
+
+/* ------------- Initial State ------------- */
+
+export const INITIAL_STATE: ITeamsState = {
+  isFetching: false,
+  fetchError: null,
+  isCreating: false,
+  createError: null
+};
+
+/* ------------- Reducers ------------- */
+
+const teamsQuery = noOpReducer;
+
+const teamsLoadStart: AppReducer<ITeamsState, undefined> = (state: ITeamsState) => {
+  return {
+    ...state,
+    isFetching: true,
+    fetchError: null
+  }
+};
+
+const teamsLoadError: AppReducer<ITeamsState, Error> = (state: ITeamsState, { payload }) => {
+  return {
+    ...state,
+    isFetching: false,
+    fetchError: payload
+  }
+};
+
+const teamsLoadSuccess: AppReducer<ITeamsState, undefined> = (state: ITeamsState) => {
+  return {
+    ...state,
+    isFetching: false,
+    fetchError: null
+  }
+};
+
+const teamsCreate = noOpReducer;
+
+const teamsCreateStart: AppReducer<ITeamsState, undefined> = (state: ITeamsState) => {
+  return {
+    ...state,
+    isCreating: true,
+    createError: null
+  }
+};
+
+const teamsCreateError: AppReducer<ITeamsState, Error> = (state: ITeamsState, { payload }) => {
+  return {
+    ...state,
+    isCreating: false,
+    createError: payload
+  }
+};
+
+const teamsCreateSuccess: AppReducer<ITeamsState, undefined> = (state: ITeamsState) => {
+  return {
+    ...state,
+    isCreating: false,
+    createError: null
+  }
+};
+/* ------------- Hookup Reducers To Types ------------- */
+
+const reducerMap: ReducerMap<typeof actions, ITeamsState> = {
+  teamsQuery,
+  teamsLoadStart,
+  teamsLoadError,
+  teamsLoadSuccess,
+  teamsCreate,
+  teamsCreateStart,
+  teamsCreateError,
+  teamsCreateSuccess
+};
+
+
+export const teamReducer = mapReducers(
+  INITIAL_STATE,
+  reducerMap,
+  actions
+);
+
+export default teamReducer;

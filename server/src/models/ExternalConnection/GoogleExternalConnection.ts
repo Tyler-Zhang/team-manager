f289624daf5eaa1159dcecf32016b1ece7fa6410
Tyler Zhang <TZTylerzhang@gmail.com>
2018-09-08 13:04:35 -0400

    improve google external connection

--- server/src/models/ExternalConnection/GoogleExternalConnection.ts
+++ server/src/models/ExternalConnection/GoogleExternalConnection.ts
@@ -1,33 +1,48 @@
 import { ExternalConnection } from './ExternalConnection';
 import { ChildEntity } from 'typeorm';
+import { Credentials } from 'google-auth-library/build/src/auth/credentials';
 
 @ChildEntity('GoogleExternalConnection')
 export class GoogleExternalConnection extends ExternalConnection {
-  get validUntil() {
+  public get expiryDate() {
     return new Date(this.data.validUntil);
   }
 
-  set validUntil(value: Date) {
+  public set expiryDate(value: Date) {
     this.data.validUntil = value.toString();
   }
 
-  get token() {
+  public get accessToken() {
     return this.data.token as string;
   }
 
-  set token(value: string) {
+  public set accessToken(value: string) {
     this.data.token = value;
   }
 
-  get refreshToken() {
+  public get refreshToken() {
     return this.data.refreshToken as string;
   }
 
-  set refreshToken(value: string) {
+  public set refreshToken(value: string) {
     this.data.refreshToken = value;
   }
 
   public get isValid() {
-    return Date.now() < this.validUntil.getTime();
+    return Date.now() < this.expiryDate.getTime();
+  }
+  
+  public setFromCredential(credentials: Credentials) {
+    this.refreshToken = credentials.refresh_token as string;
+    this.accessToken = credentials.access_token as string;
+    this.expiryDate = new Date(credentials.expiry_date as any);
+  }
+
+  public toCredential(): Credentials {
+    return {
+      access_token: this.accessToken,
+      refresh_token: this.refreshToken,
+      expiry_date: this.expiryDate.getTime()
+    }
   }
 }

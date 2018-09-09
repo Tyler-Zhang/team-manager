c1741fbebc576a238c0118c7d270d7c83e715f70
Tyler Zhang <TZTylerzhang@gmail.com>
2018-09-09 00:31:56 -0400

    Auto operation

---
+++ server/src/lib/AutoOperation.ts
@@ -0,0 +1,109 @@
+/**
+ * This lib tries to implement a similar way of running the appropriate
+ * operation for subclasses of a STI model.
+ * 
+ * There are base operations and superclassed operations. These are
+ * added to the container with decorators.
+ */
+
+const PROPERTY_NAMESPACE = '__operation_container';
+const MODEL_NAME_PROPERTY = Symbol(`${PROPERTY_NAMESPACE}_model_name`);
+const OPERATION_NAME_PROPERTY = Symbol(`${PROPERTY_NAMESPACE}_operation_name`);
+const IS_BASE_PROPERTY = Symbol(`${PROPERTY_NAMESPACE}_is_base`);
+
+/**
+ * Interface types
+ */
+interface IClass<T = any> {
+  [MODEL_NAME_PROPERTY]: string;
+  [OPERATION_NAME_PROPERTY]: string;
+  [IS_BASE_PROPERTY]: boolean;
+  name: string;
+  __proto__: IClass<any>;
+  new(...args: any[]): T;
+}
+
+interface IOperationMap {
+  klass: IClass,
+  subclasses: Record<string, IOperationMap>
+}
+
+function getOperationDescriptorKey(klass: IClass) {
+  return `${klass[OPERATION_NAME_PROPERTY]}.${klass[MODEL_NAME_PROPERTY]}`;
+}
+
+
+/**
+ * the key for the map is the name of the model that the operation
+ * should be called for
+ */
+const container: Record<string, IOperationMap> = {};
+
+export function Operation(modelName: string) {
+  return (klass: IClass) => {
+    klass[OPERATION_NAME_PROPERTY] = klass.name;
+    klass[MODEL_NAME_PROPERTY] = modelName;
+    klass[IS_BASE_PROPERTY] = true;
+
+    container[getOperationDescriptorKey(klass)] = {
+      klass,
+      subclasses: {}
+    };
+
+    return klass;
+  }
+}
+
+export function SubclassOperation(modelName: string) {
+  return (klass: IClass) => {
+    const parentClass = klass.__proto__;
+    
+    klass[OPERATION_NAME_PROPERTY] = parentClass.name;
+    klass[MODEL_NAME_PROPERTY] = modelName;
+    klass[IS_BASE_PROPERTY] = false;
+
+    const parentClassKey = getOperationDescriptorKey(klass);
+
+    if (!container[parentClassKey]) {
+      throw new Error(`${parentClassKey} not found`);
+    }
+
+    container[parentClassKey].subclasses[modelName] = {
+      klass,
+      subclasses: {}
+    };
+
+    return klass;
+  }
+}
+
+export abstract class BaseOperation {
+  public static getAppropriateClass(...args: any[]) {
+    const baseKey = getOperationDescriptorKey(
+      (this as any)[IS_BASE_PROPERTY]? (this as any).__proto__ : this
+    );
+    
+    const IOperationMapItem = container[baseKey];
+    
+    const modelType = this.getType(...args);
+
+    return (
+      IOperationMapItem.subclasses[modelType] && 
+      IOperationMapItem.subclasses[modelType].klass
+    ) || IOperationMapItem.klass;
+  }
+
+  public static getType(...args: any[]): string {
+    throw new Error('This method needs to be implemented');
+  }
+
+  public static build(...args: any[]): any {
+    const appropriateClass = this.getAppropriateClass(...args);
+
+    return appropriateClass(...args);
+  }
+
+  public static run(...args: any[]) {
+    return this.build(...args).run();
+  }
+}

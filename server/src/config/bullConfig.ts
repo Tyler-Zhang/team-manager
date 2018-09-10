8652327066fa3b9d13229f50427c2fc2c8c1ee5e
Tyler Zhang <TZTylerzhang@gmail.com>
2018-09-10 17:49:16 -0400

    bullConfig

---
+++ server/src/config/bullConfig.ts
@@ -0,0 +1,11 @@
+import Queue from 'bull';
+import * as assert from 'assert';
+
+const REDIS_URL = process.env.REDIS_URL;
+
+assert.ok(REDIS_URL, 'ENV variable REDIS_URL required!');
+
+/**
+ * Different queues that we can publish messages to
+ */
+export const syncResourceQueue = new Queue('syncResourceQueue');

5bbc15515842282bd520838a3cb84d2fb209a208
Tyler Zhang <TZTylerzhang@gmail.com>
2018-09-18 23:36:15 -0400

    update env, build files

--- client/scripts/build.sh
+++ client/scripts/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Build the static files locally
-sudo docker build
+sudo docker build . -t team-manager-client
+
+sudo docker tag team-manager-client tylerzhang/team-manager-client
+
+sudo docker push tylerzhang/team-manager-client

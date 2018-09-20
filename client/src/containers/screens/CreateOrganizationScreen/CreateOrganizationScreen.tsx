eab42c17d00befe299da667226594ae10e8aa80b
Tyler Zhang <TZTylerzhang@gmail.com>
2018-09-19 23:12:43 -0400

    improve create organization screen

--- client/src/containers/screens/CreateOrganizationScreen/CreateOrganizationScreen.tsx
+++ client/src/containers/screens/CreateOrganizationScreen/CreateOrganizationScreen.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 
 import { RouteComponentProps, withRouter } from 'react-router';
 import { compose } from 'recompose';
-import CenterInPage from '../../../components/CenterInPage/CenterInPage';
 import { withAlertContext } from '../../../compositions';
 import { IAlertProps } from '../../../compositions/withAlertContext';
 import { IMember, IOrganization, ProtoModel } from '../../../models';
 import { OrganizationService } from '../../../services';
 import SignupFormComponent from './SignupForm/SignupForm';
+
+import './CreateOrganizationScreen.css';
 
 interface ICreateOrganizationParams {
   organization: ProtoModel<IOrganization>;
@@ -17,10 +18,12 @@ interface ICreateOrganizationParams {
 class CreateOrganizationScreen extends React.Component<IAlertProps & RouteComponentProps<{}>> {
   public render() {
     return (
-      <CenterInPage width={6}>
-        <SignupFormComponent
-          onSubmit={this.onSubmit}/>
-      </CenterInPage>
+      <div className="create-organization-screen">
+        <div className="signup-form-container shadowed">
+          <SignupFormComponent
+            onSubmit={this.onSubmit}/>
+        </div>
+      </div>
     )
   }
 

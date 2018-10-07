9a70214f27695f5431c727995adb5a141eb05dea
Tyler Zhang <TZTylerzhang@gmail.com>
2018-10-07 13:03:08 -0400

    slack controller for webhook notfication

--- server/src/controllers/SlackController.ts
+++ server/src/controllers/SlackController.ts
@@ -3,7 +3,13 @@ import { JsonController, Post, BodyParam } from 'routing-controllers'
 @JsonController('/slack')
 export default class SlackController {
   @Post('/notify')
-  public async notify (@BodyParam('challenge') challenge: string) {
-    return challenge;
+  public async notify (@BodyParam('challenge') challenge?: string) {
+    /**
+     * This is used during initial slack setup to setup and verify
+     * the endpoint
+     */
+    if (challenge) {
+      return challenge;
+    }
   }
 }
